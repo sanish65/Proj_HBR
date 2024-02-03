7871ecf0b937ff1b74a969a9fe192ed79688fbaf
sanish65 <sanishmaharjan65@gmail.com>
2024-02-04 00:01:29 +0545

    new push

--- src/lead/commands/find-all-lead.command.ts
+++ src/lead/commands/find-all-lead.command.ts
@@ -1,1 +1,12 @@
 export class FindAllLeadCommand {}
+
+export interface ILead {
+  readonly lead_id: number;
+  readonly lead_name: string;
+  readonly email: string;
+  readonly lead_status: string;
+  readonly source: string;
+  readonly added_date: Date;
+  readonly updated_date: Date;
+  readonly interaction_count: string;
+}
--- src/lead/commands/handlers/find-all-lead.handler.ts
+++ src/lead/commands/handlers/find-all-lead.handler.ts
@@ -1,9 +1,10 @@
 import { Lead } from '../../entities/lead.entity';
 import { Repository, UpdateResult } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { FindAllLeadCommand } from '../find-all-lead.command';
+import { FindAllLeadCommand, ILead } from '../find-all-lead.command';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { Injectable, BadRequestException } from '@nestjs/common';
+import { Interaction } from 'interaction/entities/interaction.entity';
 
 @Injectable()
 @CommandHandler(FindAllLeadCommand)
@@ -13,13 +14,42 @@ export class FindAllLeadHandler implements ICommandHandler<FindAllLeadCommand> {
     private readonly leadRepository: Repository<Lead>,
   ) {}
 
-  async execute(command: FindAllLeadCommand): Promise<Lead[]> {
+  async execute(command: FindAllLeadCommand): Promise<ILead[]> {
     try {
-      return this.leadRepository.find({
-        order: {
-          lead_id: 'ASC',
-        },
+      const aggregatedLeadsData = await this.leadRepository
+        .createQueryBuilder('lead')
+        .leftJoin(
+          Interaction,
+          'interaction',
+          'interaction.lead_id = lead.lead_id',
+        )
+        .select([
+          'lead.lead_id',
+          'lead.lead_name',
+          'lead.lead_status',
+          'lead.email',
+          'lead.source',
+          'lead.added_date',
+          'lead.updated_date',
+          'COUNT(interaction.lead_id) AS interaction_count',
+        ])
+        .groupBy('lead.lead_id')
+        .getRawMany();
+
+      const structuredLeadsData = aggregatedLeadsData.map((data) => {
+        return {
+          lead_id: data.lead_lead_id,
+          lead_name: data.lead_lead_name,
+          email: data.lead_email,
+          lead_status: data.lead_lead_status,
+          source: data.lead_source,
+          added_date: data.lead_added_date,
+          updated_date: data.lead_updated_date,
+          interaction_count: data.interaction_count,
+        };
       });
+
+      return structuredLeadsData;
     } catch (error) {
       throw new BadRequestException(error.message);
     }
